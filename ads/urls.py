252ab35c58ec80c09de53c31e124d53173b2bd84
unknown <shout.mahmud@gmail.com>
2020-08-25 11:33:48 +0600

    working on listing page

--- ads/urls.py
+++ ads/urls.py
@@ -5,5 +5,5 @@ from .import views
 urlpatterns = [
     path('post-ads/', views.post_ads, name='post-ads'),
     path('ads-listing/', views.ads_listing, name='ads-listing'),
-    path('ads-detail/', views.ads_detail, name='ads-detail'),
+    path('ads/<int:pk>/', views.ads_detail, name='ads-detail'),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Ads
 
 # Create your views here.
 
@@ -6,10 +7,22 @@ def post_ads(request):
     return render(request, 'ads/post-ads.html')
 
 def ads_listing(request):
-    return render(request, 'ads/ads-listing.html')
+    ads_listing = Ads.objects.all()
 
-def ads_detail(request):
-    return render(request, 'ads/ads-detail.html')
+    context = {
+        'ads_listing' : ads_listing,
+    }
+
+    return render(request, 'ads/ads-listing.html', context)
+
+def ads_detail(request, pk):
+    ads_detail = get_object_or_404(Ads, pk=pk)
+
+    context = {
+        'ads_detail' : ads_detail,
+    }
+
+    return render(request, 'ads/ads-detail.html', context)
 
 
 
