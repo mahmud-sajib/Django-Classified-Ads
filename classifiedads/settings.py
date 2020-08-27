d7e64b00fa1085e51b4e4ce925f9e1107b49b9fa
mahmud-sajib <shout.mahmud@gmail.com>
2020-08-27 14:20:49 +0600

    updated stuffs

--- classifiedads/settings.py
+++ classifiedads/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '+$#4m3n(oz_n#ip-3bpl5g5ew7)v$gq66rwhgz(dune&0(2!)g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['huntingads.herokuapp.com']
 
 
 # Application definition
@@ -161,3 +162,5 @@ CKEDITOR_CONFIGS = {
         ]
     }
 }
+
+django_heroku.settings(locals())
